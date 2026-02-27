d6e738457616fc5c382291c1c79abba05a9080d3
2smrb-gpovher1507 <gpovher1507@g.educaand.es>
2026-02-27 23:45:22 +0100

    copia ramdon

--- publicar_trabajo.php
+++ publicar_trabajo.php
@@ -69,10 +69,29 @@ if (isset($_POST['publicar'])) {
                     <label data-key="label_cat">Categoría</label>
                     <select name="categoria" id="select_cat" required>
                         <option value="" data-key="opt_default">-- Selecciona una categoría --</option>
-                        <option value="Reparacion" data-key="opt_repair">Reparación</option>
-                        <option value="Configuracion" data-key="opt_config">Configuración</option>
-                        <option value="Programacion" data-key="opt_dev">Programación</option>
-                        <option value="Administracion" data-key="opt_admin">Administración</option>
+                        <option value="Desarrollo Web" data-key="opt_web_dev">Desarrollo Web</option>
+                        <option value="Desarrollo multiplataforma" data-key="opt_multi_dev">Desarrollo multiplataforma</option>
+                        <option value="Ciberseguridad" data-key="opt_cybersecurity">Ciberseguridad</option>
+                        <option value="Soporte Técnico" data-key="opt_tech_support">Soporte Técnico</option>
+                        <option value="IA y Datos" data-key="opt_ai_data">IA y Datos</option>
+                        <option value="Sistemas" data-key="opt_systems">Sistemas</option>
+
+                        <option value="Diseño Gráfico" data-key="opt_graphic_design">Diseño Gráfico</option>
+                        <option value="UI/UX" data-key="opt_ui_ux">UI/UX</option>
+                        <option value="Edición de Vídeo" data-key="opt_video_edit">Edición de Vídeo</option>
+                        <option value="Ilustración" data-key="opt_illustration">Ilustración</option>
+                        <option value="Fotografía" data-key="opt_photography">Fotografía</option>
+
+                        <option value="SEO" data-key="opt_seo">SEO</option>
+                        <option value="Community Manager" data-key="opt_community_mgr">Community Manager</option>
+                        <option value="Copywriting" data-key="opt_copywriting">Copywriting</option>
+                        <option value="Publicidad (Ads)" data-key="opt_ads">Publicidad (Ads)</option>
+                        <option value="Traducción" data-key="opt_translation">Traducción</option>
+
+                        <option value="Asistente Virtual" data-key="opt_virtual_asst">Asistente Virtual</option>
+                        <option value="Contabilidad" data-key="opt_accounting">Contabilidad</option>
+                        <option value="Consultoría Legal" data-key="opt_legal_cons">Consultoría Legal</option>
+                        <option value="Recursos Humanos" data-key="opt_hr">Recursos Humanos</option>
                     </select>
                 </div>
 
